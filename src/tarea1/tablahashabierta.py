82fac1c6eafad905394ca0f70f8fab8699e40c73
midassilver <emanuelstar400@gmail.com>
2026-04-28 08:38:30 -0600

    Se agrego la funcion Hash

--- src/tarea1/tablahashabierta.py
+++ src/tarea1/tablahashabierta.py
@@ -1,4 +1,16 @@
 from tarea1.funcion import Funcion
 
 class TablaHashAbierta(Funcion):
-    pass
+    
+    def __init__(self, table_size=101):
+        self.table_size = table_size
+        self.table = [[] for _ in range(table_size)] 
+    # 🔹 Función hash (polinomial)
+    def hash_func(self, key: str) -> int:
+        hash_value = 0
+        p = 31  
+
+        for char in key:
+            hash_value = (hash_value * p + ord(char)) % self.table_size
+
+        return hash_value
